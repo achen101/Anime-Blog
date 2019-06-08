8c5c817a583b8395e986be29731b0ff3c320261f
Andy <chandy@umich.edu>
2019-06-08 18:15:08 -0400

    fixed minor bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
 module ApplicationHelper
-  def gravatar_for(user, options = {size: 80})
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "img-circle")
-  end
-
   def genre_submit_text
     if controller.action_name == "new"
        return "Add Genre"
